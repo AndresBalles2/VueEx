34b53d2dc208c9ee775b184475a596581bd6442d
Andres Ballesteros <amballesterosr@ufpso.edu.co>
2024-05-16 15:57:11 -0500

    waos

---
+++ semaforo/src/components/Semaforo.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const arregloCarros = ref([true, true, false, true, true, true, true, true, true, true])
+const arregloMotos = ref([true, true, false, true, true, true, true, true, true, true])
+const counter = ref(0)
+
+function changeColor(index, arr, precio) {
+    if (!arr[index]) {
+        counter.value += precio
+    }
+    arr[index] = !arr[index]
+}
+
+</script>
+
+<template>
+
+    {{ counter }}
+
+    <section class=" grid grid-cols-4 p-3 gap-3">
+
+        <div @click="changeColor(index, arregloCarros, 1000)" v-for="(arr, index) in arregloCarros" class="w-full h-72"
+            :class="{ 'bg-green-500': arr }, { 'bg-red-500': !arr }">
+        </div>
+
+    </section>
+
+    <section class=" grid grid-cols-4 p-3 gap-3">
+
+        <div @click="changeColor(index, arregloMotos, 500)" v-for="(arr, index) in arregloMotos" class="w-full h-72"
+            :class="{ 'bg-green-500': arr }, { 'bg-red-500': !arr }">
+        </div>
+
+    </section>
+
+</template>
